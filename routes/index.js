b98520eb6e35a5761a7de7dc9946a017cbfb02bc
Po Chun, Lu <oceanus11034@gmail.com>
2018-09-12 19:28:56 +0800

    [Update] set filter parameters"

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const UTILS = require('../utils');
 
 
 router.get('/food', (req, res, next) => {
-  const store = UTILS.randomPickAStore(req.query.price);
+  const filter = {
+    price: req.query.price || 'low',
+  }
+  const store = UTILS.randomPickAStore(filter);
   const msg = {
     "messages": [
       {"text": store},
